300113fb3df2d76ed6b6470f339abd84a9d88289
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-06-07 21:00:40 +0100

    Added an Answer class to deserialise json object into and a controller. Controller calls a  Card Against Humanity API, I created, for an answer and returns a 200 ok response

---
+++ AnswerService/Controllers/AnswerController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using AnswerService.Model;
+
+namespace AnswerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AnswerController : ControllerBase
+    {
+        // Static Instance of HttpClient handles requests and responses
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetAnswer()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var task = client.GetStreamAsync("http://127.0.0.1:5000/answer/foodpack/");
+            var answer = await JsonSerializer.DeserializeAsync<Answer>(await task);
+            Console.WriteLine($"task:{{ { answer.Text} }}, pack: {{ {answer.Pack} }}");
+            return Ok(answer);
+        }
+    }
+}
---
+++ AnswerService/Model/Answer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace AnswerService.Model
+{
+    public class Answer
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("pack")]
+        public int Pack { get; set; }
+    }
+}
