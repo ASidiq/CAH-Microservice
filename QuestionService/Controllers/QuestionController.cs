a07092f3a035d194c1c77a7ff5ec171ede705cc9
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-06-07 20:17:33 +0100

    Added a Question class to deserialise json object into and a controller. Controller calls a  Card Against Humanity API, I created,  for questions and returns a 200 ok response

---
+++ QuestionService/Controllers/QuestionController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using QuestionService.Model;
+
+namespace QuestionService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QuestionController : ControllerBase
+    {
+        // Static Instance of HttpClient handles requests and responses
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetQuestion()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var task = client.GetStreamAsync("http://127.0.0.1:5000/question/foodpack/");
+            var question = await JsonSerializer.DeserializeAsync<Question>(await task);
+            Console.WriteLine($"task:{{ { question.Text} }}, pick:{{ {question.Pick} }}, pack: {{ {question.Pack} }}");
+            return Ok(question);
+        }
+
+
+    }
+}
---
+++ QuestionService/Model/Question.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+using System.Text.Json.Serialization;
+
+namespace QuestionService.Model
+{
+    public class Question
+    {
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+
+        [JsonPropertyName("pick")]
+        public int Pick { get; set; }
+
+        [JsonPropertyName("pack")]
+        public int Pack { get; set; }
+    }
+}
