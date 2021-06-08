1f1062daec89f836b7569b35455a5aca018b3680
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-06-08 15:37:59 +0100

    1. Added a question and answer class to deserialise json object into. 2. Added a controller. Controller calls the question and answer API which returns  a 200 ok response and the question and answer provided by the API.

---
+++ MergedService/Controllers/MergeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Text.Json;
+using MergedService.Model;
+
+namespace MergedService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MergeController : ControllerBase
+    {
+        // Static Instance of HttpClient handles requests and responses
+        private static readonly HttpClient client = new HttpClient();
+
+        [HttpGet("")]
+        public async Task<IActionResult> GetCard()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //Question
+            var questionTask = client.GetStreamAsync("https://localhost:44333/api/Question");
+            var question = await JsonSerializer.DeserializeAsync<Question>(await questionTask);
+
+            //Answer
+            var answerTask = client.GetStreamAsync("https://localhost:44337/api/Answer");
+            var answer = await JsonSerializer.DeserializeAsync<Question>(await answerTask);
+
+            string cah = question.Text.Replace("_", answer.Text.Replace(".", ""));
+
+            var card = new { question, answer, cah};
+
+            Console.WriteLine($"task:{{ { question.Text} }}, pick:{{ {question.Pick} }}, pack: {{ {question.Pack} }}");
+            return Ok(card);
+        }
+    }
+}
