a530f59df5d48bfe6a2819d38e857c1054dad2da
Abubakr Olorunlambe <abubakrolorunlambe@gmail.com>
2021-06-09 15:20:53 +0100

    Added a homepage, game and navbar component. Refactored the html and css content from app.component.html into the relevant components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { GameComponent } from './game/game.component';
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+const routes: Routes = [
+  { path: 'game', component: GameComponent, pathMatch: 'full' },
+  { path: '', component: HomepageComponent, pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    HomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
